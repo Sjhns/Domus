7c82294c611c2b6709e3154bf5028df5231897d7
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-05-10 13:50:44 -0300

    refactor(error): remover propriedade redundante

--- src/application/errors/base-error.ts
+++ src/application/errors/base-error.ts
@@ -2,6 +2,5 @@ export abstract class BaseError extends Error {
   constructor(public message: string, public statusCode: number) {
     super(message)
     this.name = this.constructor.name
-    Error.captureStackTrace(this, this.constructor)
   }
 }
