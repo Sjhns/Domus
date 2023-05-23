60bf155977d4ca276cfe6e4dce132bcbf052c418
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-05-22 21:03:17 -0300

    feat: add validação de endereço e novas propriedades da entidade

--- src/domain/entities/apartment.spec.ts
+++ src/domain/entities/apartment.spec.ts
@@ -2,15 +2,17 @@ import { describe, expect, it } from 'vitest'
 import { Apartment } from './apartment'
 
 describe('Entity apartment test', () => {
-  it('Should create new instance apartment', () => {
+  it('Should create an apartment', () => {
     const dataApartment = {
       id: '2235653265862ss',
       size: 50,
       numberOfRooms: 2,
       numberOfBathrooms: 1,
-      address: 'rua ramiro pimentel, avenida bem querer, 2563',
+      address:
+        'Alameda Franca, 579 - Jardim Bem Querer, Salvador, Bahia, Brasil',
       rent: 600,
       vacancies: 1,
+      acceptsRoommates: false,
     }
     const apartment = new Apartment(dataApartment)
 
@@ -27,10 +29,47 @@ describe('Entity apartment test', () => {
       numberOfBathrooms: -2,
       vacancies: -1,
       rent: 0,
+      acceptsRoommates: false,
     }
 
     expect(() => new Apartment(invalidProps)).toThrowError(
-      'Informações não são validas',
+      'Informações não são válidas',
+    )
+  })
+
+  it('Should throw an error when an invalid address is passed', () => {
+    const invalidAddress = {
+      id: '1',
+      address: '123 Main Street, 42 - Invalid Neighborhood, Invalid City, AA',
+      size: 100,
+      numberOfRooms: 2,
+      numberOfBathrooms: 1,
+      vacancies: 1,
+      rent: 1000,
+      acceptsRoommates: false,
+    }
+
+    expect(() => new Apartment(invalidAddress)).toThrowError(
+      'Endereço inválido. O formato correto é "nome da rua, número - bairro, cidade, estado, país".',
+    )
+  })
+
+  it('Should throw an error when invalid properties are passed', () => {
+    const invalidProps = {
+      id: '1',
+      address:
+        'Alameda Franca, 579 - Jardim Bem Querer, Salvador, Bahia, Brasil',
+      size: 100,
+      numberOfRooms: 2,
+      numberOfBathrooms: 1,
+      vacancies: 1,
+      rent: 1000,
+      acceptsRoommates: true,
+      maxRoommates: 0,
+    }
+
+    expect(() => new Apartment(invalidProps)).toThrowError(
+      'Informações não são válidas',
     )
   })
 })
--- src/domain/entities/apartment.ts
+++ src/domain/entities/apartment.ts
@@ -6,26 +6,50 @@ type ApartmentProps = {
   numberOfBathrooms: number
   vacancies: number // vagas de carros
   rent: number
+  acceptsRoommates: boolean // indica se aceita colegas de quarto
+  maxRoommates?: number // número máximo de pessoas que podem compartilhar o apartamento
 }
 
 export class Apartment {
-  constructor(public props: Required<ApartmentProps>) {
+  constructor(public props: ApartmentProps) {
     this.validateProperties()
+    this.validateAddressPattern()
   }
 
   validateProperties(): void {
-    const { size, numberOfBathrooms, numberOfRooms, vacancies, rent } =
-      this.props
+    const {
+      size,
+      numberOfBathrooms,
+      numberOfRooms,
+      vacancies,
+      rent,
+      acceptsRoommates,
+      maxRoommates,
+    } = this.props
 
     const validate =
       size > 0 &&
       numberOfRooms > 0 &&
       numberOfBathrooms > 0 &&
       vacancies >= 0 &&
-      rent >= 0
+      rent >= 0 &&
+      (acceptsRoommates ? maxRoommates !== undefined && maxRoommates > 0 : true)
 
     if (!validate) {
-      throw new Error('Informações não são validas')
+      throw new Error('Informações não são válidas')
+    }
+  }
+
+  validateAddressPattern(): void {
+    const { address } = this.props
+
+    const addressPattern =
+      /^[A-Za-z0-9\s]+,\s[0-9]+\s-\s[A-Za-z0-9\s]+,\s[A-Za-z\s]+,\s[A-Za-z\s]+,\s[A-Za-z\s]+$/
+
+    if (!addressPattern.test(address)) {
+      throw new Error(
+        'Endereço inválido. O formato correto é "nome da rua, número - bairro, cidade, estado, país".',
+      )
     }
   }
 }
