72d640f2f547598c71a4357df8305eb9d2632b6b
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-05-31 18:43:45 -0300

    refactor: concretados os contratos da camada de application

--- src/domain/repository/apartment-repository.ts
+++
@@ -1,9 +0,0 @@
-import { SaveApartmentDTOInput } from '@/application/dtos/input-save-apartment'
-
-export interface ApartmentRepositoryContract<T> {
-  save: (input: SaveApartmentDTOInput) => Promise<T>
-  findAll: () => Promise<T[]>
-  findOne: (id: string) => Promise<T>
-  update: (id, input: SaveApartmentDTOInput) => Promise<T>
-  remove: (id: string) => Promise<void>
-}
