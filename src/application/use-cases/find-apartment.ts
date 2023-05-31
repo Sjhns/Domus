f1104aa1b64cbe607f2847cbb27b88f219d0729b
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-05-31 18:30:18 -0300

    feat: adicionar caso de uso para obter apartamento

--- src/application/use-cases/find-apartment.ts
+++ src/application/use-cases/find-apartment.ts
@@ -1,3 +1,25 @@
-export class FindApartment {
-  async execute() {}
+import { ApartmentRepositoryContract } from '@/application/contracts/apartment-repository'
+import { ApartmentModel } from '../contracts/apartment-model'
+import { ApartmentOutput } from '../dtos/output-apartment'
+import { FindApartmentUseCaseContract } from '@/domain/contracts/apartment-useCase'
+
+export class FindApartmentUseCase implements FindApartmentUseCaseContract {
+  constructor(
+    private readonly apartmentRepository: ApartmentRepositoryContract<
+      ApartmentModel,
+      ApartmentOutput
+    >,
+  ) {}
+
+  async all(): Promise<ApartmentOutput[]> {
+    const output = await this.apartmentRepository.findAll()
+
+    return output
+  }
+
+  async one(id: string): Promise<ApartmentOutput> {
+    const output = await this.apartmentRepository.findOne(id)
+
+    return output
+  }
 }
