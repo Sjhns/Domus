e869611efba8202c1deca90cd672e376de73f8e5
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-06-05 17:13:02 -0300

    feta: header

---
+++ src/frameworks/mobile/src/components/layout/header.tsx
@@ -0,0 +1,21 @@
+import { Image, Text, View } from 'react-native'
+
+export const Header = () => {
+  return (
+    <View className="flex-row justify-between items-center">
+      <View>
+        <Text className="font-medium text-gray-600">Olá,</Text>
+        <Text className="text-3xl">Johnson</Text>
+      </View>
+
+      <View>
+        <Image
+          className="w-10 h-10 rounded-full"
+          source={{
+            uri: 'https://avatars.githubusercontent.com/u/108435026?v=4',
+          }}
+        />
+      </View>
+    </View>
+  )
+}
