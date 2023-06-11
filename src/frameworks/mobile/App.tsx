0808f842f233ec0f70d7e604edbb8ee56d7f7bfa
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-06-11 19:15:33 -0300

    feat: alterar background do app

--- src/frameworks/mobile/App.tsx
+++ src/frameworks/mobile/App.tsx
@@ -5,8 +5,8 @@ import {} from 'expo'
 
 export default function App() {
   return (
-    <SafeAreaView className={`flex-1 mt-[24px]`}>
-      <StatusBar style="auto" />
+    <SafeAreaView className="flex-1 mt-[24px] bg-[#ecf2f8] text-white">
+      <StatusBar style="light" translucent={true} backgroundColor='#0d1117' />
       <ScrollView className="flex-1">
         <Home />
       </ScrollView>
