ab9772019bc07a9f932d2d606ac5041bc15722d1
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-06-11 19:18:35 -0300

    feat(Home): Adicionar componentes CarouselSmallCards e CarouselBigCards

--- src/frameworks/mobile/src/screens/home.tsx
+++ src/frameworks/mobile/src/screens/home.tsx
@@ -1,15 +1,18 @@
 import { Text, View } from 'react-native'
 import { Header } from '../components/layout/header'
 import { BarOptions } from '../components/layout/bar-options'
-import { CarouselImmobile } from '../components/layout/carousel-immobile'
+import { CarouselSmallCards } from '../components/layout/carousel-small-cards'
+import { CarouselBigCards } from '../components/layout/carousel-big-cards'
 
 export const Home = () => {
   return (
-    <View className="flex-1 px-3 pt-3">
+    <View className="flex-1 px-3 pt-3 ">
       <Header />
       <Text className="mt-[3px]">Vamos começar a explorar?</Text>
       <BarOptions />
-      <CarouselImmobile />
+      <CarouselSmallCards />
+      <CarouselBigCards />
+      <View className="mt-28" />
     </View>
   )
 }
