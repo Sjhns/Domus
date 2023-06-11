ab1ec7949642e4f29cfe593520696add9d0fb427
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-06-11 19:19:03 -0300

    feat: adicionar componente CarouselBigCards

---
+++ src/frameworks/mobile/src/components/layout/carousel-big-cards.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+
+import { Carousel } from '../common/carousel'
+import { properties } from './data-fake'
+import { BigCard } from './big-card'
+
+export const CarouselBigCards = () => {
+  return (
+    <View>
+      <View className="mt-5 mb-3">
+        <Text className="text-xl font-medium">As escolhas mais procuradas</Text>
+      </View>
+
+      <Carousel
+        data={properties}
+        spaceEachItem={10}
+        renderItem={({ item }) => <BigCard {...item} />}
+      />
+    </View>
+  )
+}
