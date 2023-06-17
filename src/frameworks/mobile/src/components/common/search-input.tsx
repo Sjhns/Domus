f06f292e8d1309e6c758a6890bd636b07aa1b2e3
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-06-17 17:19:54 -0300

    style: alterado tamanho do texto do input

---
+++ src/frameworks/mobile/src/components/common/search-input.tsx
@@ -0,0 +1,19 @@
+import { TextInput, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
+export const SearchInput = () => {
+  return (
+    <View
+      className={`bg-[#F2F2F3] rounded-full border-[.8px] 
+    border-[#E3E3E7] h-[53px] overflow-hidden  flex-row items-center justify-between px-4`}
+    >
+      <Ionicons name="search-outline" size={24} color={'#1A1E25'} />
+      <TextInput
+        placeholder="Search address, city, location..."
+        className="text-[#3a3b40] flex-1 h-full px-1 bg-transparent 
+        outline-none text-[17px]"
+      />
+      <Ionicons name="settings-outline" size={24} color={'#1A1E25'} />
+    </View>
+  )
+}
