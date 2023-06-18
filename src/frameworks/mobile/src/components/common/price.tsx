af6918cf2a2d8999f54c0ac1b08333ba8d12f54f
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-06-18 13:09:30 -0300

    feat: adicionar opção de favoritar no componente preço

--- src/frameworks/mobile/src/components/common/price.tsx
+++ src/frameworks/mobile/src/components/common/price.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 type PriceProps = {
   price: number
@@ -7,15 +8,17 @@ type PriceProps = {
 
 export const Price = ({ period, price }: PriceProps) => {
   return (
-    <View className="flex-row items-center mt-auto">
-      <Text className="text-[#1A1E25] text-xl tracking-widest font-semibold ">
-        R${price}
-      </Text>
-      <View>
+    <View className="flex-row items-center justify-between mt-auto pt-1">
+      <View className="flex-row items-center">
+        <Text className="text-[#1A1E25] text-xl tracking-widest font-semibold ">
+          R${price}
+        </Text>
         <Text className="text-[#7D7F88] font-medium pt-[6px] text-sm">
           /{period}
         </Text>
       </View>
+
+      <Ionicons name="heart-outline" size={28} color="#7D7F88" />
     </View>
   )
 }
