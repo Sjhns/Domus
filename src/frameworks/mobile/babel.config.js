234db59c85c5c3c383bc77185df506eedf68e4f0
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-07-15 19:52:30 -0300

    chore: adicionar tamagui para mobile

--- src/frameworks/mobile/babel.config.js
+++ src/frameworks/mobile/babel.config.js
@@ -1,7 +1,26 @@
+process.env.TAMAGUI_TARGET = 'native'
+
 module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
+    plugins: [
+      'nativewind/babel',
+      [
+        'transform-inline-environment-variables',
+        {
+          include: 'TAMAGUI_TARGET',
+        },
+      ],
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   }
 }
--- src/frameworks/mobile/src/main.tsx
+++ src/frameworks/mobile/src/main.tsx
@@ -3,13 +3,28 @@ import { SafeAreaView } from 'react-native'
 
 import { Navigator } from './config/navigator/navigator'
 import { COLORS } from './theme/colors'
+import { useFonts } from 'expo-font'
+
+import config from '../tamagui.config'
+import { TamaguiProvider } from 'tamagui'
 
 export const App = () => {
-  return (
-    <SafeAreaView className="flex-1 mt-[24px]">
-      <StatusBar style="auto" backgroundColor={COLORS.background} />
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  })
 
-      <Navigator />
-    </SafeAreaView>
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <TamaguiProvider config={config}>
+      <SafeAreaView className="flex-1 mt-[24px]">
+        <StatusBar style="auto" backgroundColor={COLORS.background} />
+
+        <Navigator />
+      </SafeAreaView>
+    </TamaguiProvider>
   )
 }
