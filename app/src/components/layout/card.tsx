37c44b7eebec20626c0d214aad161c9a2c3468ab
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-08-08 19:55:16 -0300

    style: fix width of the horizontal card

--- app/src/components/layout/card.tsx
+++ app/src/components/layout/card.tsx
@@ -1,6 +1,7 @@
-import { ReactElement } from 'react'
-import { Image as ImageNative, View, Text } from 'react-native'
-import { FontAwesome } from '@expo/vector-icons'
+import { View, Image as ImageNative, Text, Pressable } from 'react-native'
+import { styled } from 'nativewind'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { ReactElement, useState } from 'react'
 
 type ImmobileCardProps = {
   title: string
@@ -12,55 +13,14 @@ type ImmobileCardProps = {
   period: string
 }
 
-export const HorizontalCard = () => {
+const Image = ({ source }: { source: string }) => {
   return (
-    <View
-      className="h-48 w-[360] bg-white rounded-md overflow-hidden flex-row shadow-lg 
-    border border-[#a4a1a13f] bg-yellow-500"
-    >
-      <HorizontalCardHeader>
-        <Image source="https://www.lopes.com.br/blog/wp-content/uploads/2017/06/interior-2400372_1920.jpg" />
-      </HorizontalCardHeader>
-
-      <View>
-        <HorizontalCardBody>
-          <View className="flex-row items-center space-x-1">
-            <FontAwesome name="star" size={17} color={'#FFBF75'} />
-            <View className="flex-row">
-              <Text>4.3</Text>
-              <Text className="ml-1 text-gray-500">(73)</Text>
-            </View>
-          </View>
-
-          <View className=" flex-1">
-            <Text
-              numberOfLines={2}
-              ellipsizeMode="tail"
-              className="text-lg leading-6 text-[#1A1E25] font-normal"
-            >
-              Pent House Apartment
-            </Text>
-            <View className="flex-row items-center gap-x-[2px] mt-[2px]">
-              <Text className="text-[#7D7F88] text-[12px] tracking-widest">
-                Main Street, Downtown
-              </Text>
-            </View>
-          </View>
-        </HorizontalCardBody>
-        <HorizontalCardFooter>
-          <View className="flex-row items-center space-x-5">
-            <View className="flex-row space-x-2 items-center">
-              <FontAwesome name="bed" size={17} color={'#7D7F88'} />
-              <Text>{3} quartos</Text>
-            </View>
-            <View className="flex-row space-x-2 items-center">
-              <FontAwesome name="home" size={17} color={'#7D7F88'} />
-              <Text>{230} m²</Text>
-            </View>
-          </View>
-        </HorizontalCardFooter>
-      </View>
-    </View>
+    <ImageNative
+      source={{
+        uri: source,
+      }}
+      className="w-full h-full object-cover object-center"
+    />
   )
 }
 
@@ -77,18 +37,7 @@ type HorizontalCardBodyProps = {
 }
 
 const HorizontalCardBody = ({ children }: HorizontalCardBodyProps) => {
-  return <View className="p-3 bg-green-600 flex-1 w-full">{children}</View>
-}
-
-const Image = ({ source }: { source: string }) => {
-  return (
-    <ImageNative
-      source={{
-        uri: source,
-      }}
-      className="w-full h-full object-cover"
-    />
-  )
+  return <View className="p-3">{children}</View>
 }
 
 type HorizontalCardFooterProps = {
@@ -96,5 +45,95 @@ type HorizontalCardFooterProps = {
 }
 
 const HorizontalCardFooter = ({ children }: HorizontalCardFooterProps) => {
-  return <View className="p-3 bg-red-600">{children}</View>
+  return <View className="p-3 space-y-6">{children}</View>
+}
+
+const StyledView = styled(View)
+
+export const HorizontalCard = () => {
+  const [isFavorite, setIsFavorite] = useState(true)
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite)
+  }
+
+  return (
+    <StyledView
+      className="h-48 w-[360] bg-white rounded-md overflow-hidden flex-row shadow-lg 
+    border border-[#a4a1a13f]"
+      style={{
+        display: isFavorite ? 'flex' : 'none',
+      }}
+    >
+      <HorizontalCardHeader>
+        <Image source="https://www.lopes.com.br/blog/wp-content/uploads/2017/06/interior-2400372_1920.jpg" />
+      </HorizontalCardHeader>
+
+      <View className="flex-1 justify-between">
+        <HorizontalCardBody>
+          <View className="flex-row items-center space-x-1 mb-2">
+            <FontAwesome name="star" size={17} color={'#FFBF75'} />
+            <View className="flex-row">
+              <Text>4.3</Text>
+              <Text className="ml-1 text-gray-500">(73)</Text>
+            </View>
+          </View>
+
+          <View className="">
+            <Text
+              numberOfLines={2}
+              ellipsizeMode="tail"
+              className="text-lg leading-6 text-[#1A1E25] font-normal"
+            >
+              Pent House Apartment
+            </Text>
+            <View className="flex-row items-center gap-x-[2px] mt-[2px]">
+              <Text className="text-[#7D7F88] text-[12px] tracking-widest">
+                Main Street, Downtown
+              </Text>
+            </View>
+          </View>
+        </HorizontalCardBody>
+
+        <HorizontalCardFooter>
+          <View className="flex-row items-center space-x-5">
+            <View className="flex-row space-x-2 items-center">
+              <FontAwesome name="bed" size={17} color={'#7D7F88'} />
+              <Text>{3} quartos</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <FontAwesome name="home" size={17} color={'#7D7F88'} />
+              <Text>{230} m²</Text>
+            </View>
+          </View>
+
+          <View className="flex-row items-center justify-between mt-auto">
+            <View className="flex-row items-center">
+              <Text className="text-[#1A1E25] text-xl tracking-widest font-semibold ">
+                R$ {1000}
+              </Text>
+              <Text className="text-[#7D7F88] font-medium pt-[6px] text-sm">
+                /{'mês'}
+              </Text>
+            </View>
+
+            {isFavorite ? (
+              <Pressable onPress={toggleFavorite}>
+                <Ionicons name="heart-sharp" size={28} color="#ff0000" />
+              </Pressable>
+            ) : (
+              <Pressable onPress={toggleFavorite}>
+                <Ionicons
+                  name="heart-outline"
+                  size={28}
+                  color="#7D7F88"
+                  onPress={toggleFavorite}
+                />
+              </Pressable>
+            )}
+          </View>
+        </HorizontalCardFooter>
+      </View>
+    </StyledView>
+  )
 }
--- app/src/screens/saved.tsx
+++ app/src/screens/saved.tsx
@@ -1,4 +1,4 @@
-import { View, ScrollView, FlatList } from 'react-native'
+import { View, FlatList } from 'react-native'
 import { SearchInput } from '../components/common/search-input'
 
 import { HorizontalCard } from '../components/layout/card'
@@ -6,37 +6,19 @@ import { BlankSpace } from '../components/common/space-white'
 
 export const Saved = () => {
   const id = ['1', '2', '3', '4']
+
   return (
     <View className="flex-1 pt-7 px-4 bg-base pb-24">
       <SearchInput />
 
-      <BlankSpace size={30} />
-
-      {/* <View className="items-center justify-center">
-          {id.map((item) => (
-            <Fragment key={item}>
-              <BigImmobileCard
-                key={item}
-                title="Casa com 2 quartos e 1 banheiro"
-                location="Rua das Margaridas, 100"
-                price={1000}
-                image="https://www.lopes.com.br/blog/wp-content/uploads/2017/06/interior-2400372_1920.jpg"
-                rooms={2}
-                squareMeters={100}
-                period="Mês"
-              />
-
-              <View className="h-6" />
-            </Fragment>
-          ))}
-        </View> */}
+      <BlankSpace size={15} />
 
       <FlatList
         showsVerticalScrollIndicator={false}
         data={id}
         renderItem={() => <HorizontalCard />}
-        ItemSeparatorComponent={() => <BlankSpace size={17} />}
-        // disableVirtualization={false}
+        className="last:mb-20"
+        ItemSeparatorComponent={() => <BlankSpace size={10} />}
       />
     </View>
   )
