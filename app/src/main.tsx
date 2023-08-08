ed40e8182a28b14d6d784fb1750210813241b0a0
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-08-08 20:57:35 -0300

    style: change the background color of status bar

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,7 +5,7 @@ import { Navigator } from './config/navigator/navigator'
 export const App = () => {
   return (
     <View className="flex-1 pt-6">
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor="#FCFCFC" />
       <Navigator />
     </View>
   )
